ce9e5c278998a94057b528ef296f6fb663e0ba2b
Rifat <remixboyrifat@gmail.com>
2025-02-04 12:35:26 +0600

    update base url

--- src/app/fetchers/api/baseApi.jsx
+++ src/app/fetchers/api/baseApi.jsx
@@ -1,7 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:9000/api",
+  //baseUrl: "http://localhost:9000/api",
+  baseUrl: "https://tracking-system-backend-self.vercel.app/api",
   credentials: "include",
   prepareHeaders(headers, { getState }) {
     const token = getState().auth.token;
